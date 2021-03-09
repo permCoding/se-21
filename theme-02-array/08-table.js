798c44a48ac62ee05882217fc835f0a6e6c1ecdf
Andrey <ttxiom@gmail.com>
2021-03-09 23:33:57 +0500

    up

---
+++ theme-02-array/08-table.js
@@ -0,0 +1,11 @@
+const languages = [
+    {id: 1, name: JavaScript, rating: 4.44},
+    {id: 2, name: C#, rating: 11.6},
+    {id: 3, name: Python, rating: 7.56},
+    {id: 4, name: Java, rating: 9.01},
+    {id: 5, name: PHP, rating: 8.52}
+];
+
+console.log(languages);
+
+console.table(languages);
