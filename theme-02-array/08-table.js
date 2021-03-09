8b9fef716eaa607a29fa3f50fdbe418a66da8708
Andrey <ttxiom@gmail.com>
2021-03-09 23:40:20 +0500

    up

--- theme-02-array/08-table.js
+++ theme-02-array/08-table.js
@@ -1,11 +1,21 @@
 const languages = [
-    {id: 1, name: JavaScript, rating: 4.44},
-    {id: 2, name: C#, rating: 11.6},
-    {id: 3, name: Python, rating: 7.56},
-    {id: 4, name: Java, rating: 9.01},
-    {id: 5, name: PHP, rating: 8.52}
+    {id: 1, 'name lang': 'JavaScript', rating: 4.44},
+    {id: 2, 'name lang': 'C#', rating: 11.6},
+    {id: 3, 'name lang': 'Python', rating: 7.56},
+    {id: 4, 'name lang': 'Java', rating: 9.01},
+    {id: 5, 'name lang': 'PHP', rating: 8.52}
 ];
 
 console.log(languages);
 
 console.table(languages);
+
+languages
+    .sort((a, b) => b.rating - a.rating)
+    .forEach(x => console.log(x['name lang'])); // если имя поля с пробелом
+
+languages
+    .sort((a, b) => b.rating - a.rating)
+    .forEach(x => console.log(x.rating)); // если имя поля без пробела
+
+console.table(languages.sort((a, b) => b.rating - a.rating));
