8d0dc03c50f38711dd3b8322eb2e92c1fceffdf1
Андрей Беляков <ttxiom@gmail.com>
2021-05-13 12:28:13 +0500

    Update 05-count.js

--- theme-06-MySQL/part2-SQL/05-count.js
+++ theme-06-MySQL/part2-SQL/05-count.js
@@ -7,16 +7,16 @@ const get_conn = require('./modules/utils').get_conn;
 
 const conn = get_conn();
 
-conn.promise()
-    .query("SELECT COUNT(*) FROM abiturs")
-    .then(([rows,fields]) => {
-        console.log(rows, '\n', fields);
-    }) // rows => [ TextRow { 'COUNT(*)': 15 } ] 
-    .then(conn.end());
-
 // conn.promise()
 //     .query("SELECT COUNT(*) FROM abiturs")
-//     .then(([rows]) => { return rows[0]['COUNT(*)']; }) // отсюда
-//     .then((count) => { console.log('count =', count) }) // сюда
-//     .catch((err) => { console.error(err) })
+//     .then(([rows,fields]) => {
+//         console.log(rows, '\n', fields);
+//     }) // rows => [ TextRow { 'COUNT(*)': 15 } ] 
 //     .then(conn.end());
+
+conn.promise()
+    .query("SELECT COUNT(*) FROM abiturs")
+    .then(([rows]) => { return rows[0]['COUNT(*)']; }) // отсюда
+    .then((count) => { console.log('count =', count) }) // сюда
+    .catch((err) => { console.error(err) })
+    .then(conn.end());
